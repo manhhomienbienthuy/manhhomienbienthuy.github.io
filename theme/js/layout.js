1594d39a369c826d091789540559e78ad60b81cc
manhhomienbienthuy <1755796+manhhomienbienthuy@users.noreply.github.com>
2026-08-27 00:34:12 +0000

    fac5d90

--- theme/js/layout.js
+++ theme/js/layout.js
@@ -3,4 +3,4 @@
  * Copyright (C) 2016-present manhhomienbienthuy
  * This file is distributed under the same license as the website.
  */
-addEventListener("load",()=>{return r=window,n=document,(e=()=>{var e,t,l=n.querySelector(".sm\\:h-\\[3500px\\]");l&&(640<=r.innerWidth?(e=[].reduce.call(n.getElementsByClassName("sm:order-1"),(e,t)=>e+t.clientHeight+24,0),t=[].reduce.call(n.getElementsByClassName("sm:order-2"),(e,t)=>e+t.clientHeight+24,0),l.style.height=Math.max(e,t)+"px"):l.style.height=null)})(),void(r.onresize=e);var r,n,e});
+addEventListener("load",()=>{return t=window,n=document,(e=()=>{var e=n.querySelector(".entry-list");if(e)if(640<=t.innerWidth){let l=0,i=0;n.querySelectorAll(".entry-list .entry-item").forEach((e,t)=>{console.log("debug",t,e.clientHeight),t%2?i+=e.clientHeight+24:l+=e.clientHeight+24}),console.log("debug",l,i),e.style.height=Math.max(l,i)+"px"}else e.style.height=null})(),void(t.onresize=e);var t,n,e});
