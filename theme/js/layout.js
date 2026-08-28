6dc179fa4190405ce9e5d355973e15ed265fd07e
manhhomienbienthuy <1755796+manhhomienbienthuy@users.noreply.github.com>
2026-08-28 09:01:12 +0000

    876bbe5

--- theme/js/layout.js
+++ theme/js/layout.js
@@ -3,4 +3,4 @@
  * Copyright (C) 2016-present manhhomienbienthuy
  * This file is distributed under the same license as the website.
  */
-addEventListener("load",()=>{var e=()=>{var e=document.querySelector(".article-list");if(e)if(768<=window.innerWidth){let i=[0,0];e.querySelectorAll(".article-item").forEach((e,t)=>{i[t%2]+=e.clientHeight+24}),e.style.height=Math.max(...i)+"px"}else e.style.height=null};e(),window.onresize=e});
+addEventListener("load",()=>{let e=document.querySelector(".article-list");var t;e&&((t=()=>{if(window.innerWidth<768)e.style.height="";else{let i=[0,0];e.querySelectorAll(".article-item").forEach((e,t)=>{i[t%2]+=e.clientHeight+24}),e.style.height=Math.max(i[0],i[1])+"px"}})(),addEventListener("resize",t))});
